bb72e33ce3a4f58cf8310f7c4ff68388bf5e7bc4
Ahmed Faraz Shaikh <123117660+ahmedfarazsyk@users.noreply.github.com>
2024-04-03 01:14:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ tfidf = pickle.load(open("vectorizer.pkl", "rb"))
 model = pickle.load(open("model.pkl", "rb"))
 
 
-st.title("Email/SMS Classifier")
+st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
